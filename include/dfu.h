65bde13be9653ec6a5fdf8d14ed366f565d398c7
Łukasz Majewski <l.majewski@samsung.com>
2013-09-10 15:29:24 +0200

    dfu: Find DFU alt setting number by passing its name
    New function - dfu_get_alt() has been added to dfu core. If proper
    alt setting is present, this function returns its number
    corresponding to passed name.
    
    Change-Id: Icd75f3aa3a6f6e306c77b28cabe620e4e6a253ea
    Signed-off-by: Lukasz Majewski <l.majewski@samsung.com>

--- include/dfu.h
+++ include/dfu.h
@@ -121,6 +121,7 @@ const char *dfu_get_dev_type(enum dfu_device_type t);
 const char *dfu_get_layout(enum dfu_layout l);
 struct dfu_entity *dfu_get_entity(int alt);
 char *dfu_extract_token(char** e, int *n);
+int dfu_get_alt(const char *name);
 
 int dfu_read(struct dfu_entity *de, void *buf, int size, int blk_seq_num);
 int dfu_write(struct dfu_entity *de, void *buf, int size, int blk_seq_num);
