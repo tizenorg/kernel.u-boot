b16992c31473e4109762b6dad4bcb9cdcd60e3b7
Inha Song <ideal.song@samsung.com>
2013-10-02 18:05:26 +0900

    TRATS2: config: add some definitions for usbdown command
    Add some definitions for usbdown command support in TRATS2 config file.
    
    Change-Id: I04a6a22f04ae3fc0049aaf800a08fc9aebeb7194
    Signed-off-by: Inha Song <ideal.song@samsung.com>

--- include/configs/trats2.h
+++ include/configs/trats2.h
@@ -131,6 +131,12 @@
 #define CONFIG_G_DNL_MANUFACTURER "Samsung"
 
 /* To use the TFTPBOOT over USB, Please enable the CONFIG_CMD_NET */
+/* USB Downloader */
+#define CONFIG_CMD_USBDOWN
+#define CONFIG_S5P_USB_DMA	/* DMA mode */
+#define CONFIG_SYS_DOWN_ADDR	0x53000000
+#define CONFIG_SYS_BACKUP_ADDR	CONFIG_SYS_DOWN_ADDR + (80 << 20)
+
 #undef CONFIG_CMD_NET
 
 /* MMC */
