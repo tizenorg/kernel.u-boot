bfd9e5a902d9d44d458b2dc178e5351dfa5915f6
Inha Song <ideal.song@samsung.com>
2013-10-25 18:00:06 +0900

    TRTAS2:config: Clean up unnecessary define
    Clean up unused define.
    
    Signed-off-by: Inha Song <ideal.song@samsung.com>

--- include/configs/trats2.h
+++ include/configs/trats2.h
@@ -149,11 +149,6 @@
 /* To use the TFTPBOOT over USB, Please enable the CONFIG_CMD_NET */
 /* USB Downloader */
 #define CONFIG_CMD_USBDOWN
-#define CONFIG_S5P_USB_DMA	/* DMA mode */
-#define CONFIG_USB_DEVGURU	/* USB driver */
-#define CONFIG_S5P_USB_NON_ZLP	/* NON-ZLP mode on DEVGURU */
-#define CONFIG_SYS_DOWN_ADDR	0x53000000
-#define CONFIG_SYS_BACKUP_ADDR	CONFIG_SYS_DOWN_ADDR + (80 << 20)
 
 #undef CONFIG_CMD_NET
 
