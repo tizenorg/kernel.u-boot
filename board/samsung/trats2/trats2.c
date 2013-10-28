715353c6e7aeb940fd88fedc598dd30d5cfe6c34
Inha Song <ideal.song@samsung.com>
2013-10-28 20:42:01 +0900

    TRATS2: Add function to check board signature by using file name
    Add some function to check board signature by using file name.
    
    Change-Id: Ifd83914ece5be14373c765559afb37b050be628f
    Signed-off-by: Inha Song <ideal.song@samsung.com>

--- board/samsung/trats2/trats2.c
+++ board/samsung/trats2/trats2.c
@@ -50,6 +50,7 @@
 #ifdef CONFIG_CMD_PIT
 #include <pit.h>
 #endif
+#include <sighdr.h>
 DECLARE_GLOBAL_DATA_PTR;
 
 static struct exynos4x12_gpio_part1 *gpio1;
@@ -701,3 +702,59 @@ struct ums_board_info *board_ums_init(unsigned int dev_num, unsigned int offset,
 	return &ums_board;
 }
 #endif
+
+static int get_board_signature(phys_addr_t base_addr,
+			       phys_size_t size, struct sig_header *hdr)
+{
+	hdr = (struct sig_header *)(base_addr + size - HDR_SIZE);
+
+	if (hdr->magic != HDR_BOOT_MAGIC) {
+		debug("can't found signature\n");
+		return -1;
+	}
+
+	return 0;
+}
+
+int check_board_signature(char *fname, phys_addr_t dn_addr)
+{
+	struct sig_header bh_target;
+	struct sig_header bh_addr;
+	phys_size_t part_size;
+	int pit_idx;
+	int ret;
+
+	/* u-boot-mmc.bin */
+	if (strcmp(fname, PARTS_BOOTLOADER"-mmc.bin"))
+		return 0;
+
+	pit_idx = get_pitpart_id_by_filename(fname);
+
+	if (!pit_idx)
+		return 0;
+
+	part_size = get_pitpart_size(pit_idx);
+
+	ret = get_board_signature((phys_addr_t)CONFIG_SYS_TEXT_BASE,
+				  part_size, &bh_target);
+
+	/* can't found signature in target - download continue */
+	if (ret)
+		return 0;
+
+	ret = get_board_signature(dn_addr, part_size, &bh_addr);
+
+	/* can't found signature in address - download stop */
+	if (ret)
+		return -1;
+
+	if (strncmp(bh_target.bd_name, bh_addr.bd_name,
+		    ARRAY_SIZE(bh_target.bd_name))) {
+		debug("board name Inconsistency\n");
+		return -1;
+	}
+
+	debug("board signature check OK\n");
+
+	return 0;
+}
